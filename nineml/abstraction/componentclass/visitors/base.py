3dd58421c754d1e5fe00f76bcaa43fe9ffa52576
Tom Close <tclose@oist.jp>
2015-06-11 16:10:26 +0900

    fixed up unit-tests after rebase

--- nineml/abstraction/componentclass/visitors/base.py
+++ nineml/abstraction/componentclass/visitors/base.py
@@ -17,11 +17,7 @@ class ComponentActionVisitor(ComponentVisitor):
 
     def __init__(self, require_explicit_overrides=True):
         self.require_explicit_overrides = require_explicit_overrides
-<<<<<<< refactor_v2
-        self._scopes = []        
-=======
         self._scopes = []
->>>>>>> HEAD~1
 
     def visit_componentclass(self, component_class, **kwargs):
         self.action_componentclass(component_class, **kwargs)
